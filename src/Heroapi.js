48fd95b3af04c4b13aa82d1325e3016804144a16
krankie <foresvives10@gmail.com>
2020-07-15 11:56:15 +0200

    prueba

--- src/Heroapi.js
+++ src/Heroapi.js
@@ -3,24 +3,36 @@
 
 function getHeroes(){
     const heroes = document.querySelector('.heroes');
-
-    var arr = [];
+    const col1 = document.querySelector('.col1')
+    const col2 = document.querySelector('.col2')
        
-            for(var i = 1; i<731;i++){
-         fetch(`https://superheroapi.com/api/599075271045222/${i}`)
+            for(var i = 1; i<3;i++){
+
+         fetch(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/599075271045222/${i}`)
         .then((response) =>{
             return response.json()
         })
 
         .then((data) => {
             const article = document.createElement('article');
-            article.innerHTML = `<img src = "${data.image.url}" alt = "${data.name}"/>
-                                 <h3>${data.name}</h3>   `
-    
+            article.innerHTML = `<h3>${data.name}</h3>
+            <img src = "${data.image.url}" alt = "${data.name}"/>
+            <ul>
+            <li>intelligence:${data.powerstats.intelligence}</li>
+               <li>strength:${data.powerstats.strength}</li>
+               <li>speed:${data.powerstats.speed}</li>
+               <li>durability:${data.powerstats.durability}</li>
+               <li>power:${data.powerstats.power}</li>
+               
+               <li>combat:${data.powerstats.combat}</li> </ul>`
+                
 
-            section.appendChild(article)
+               article.classList.add('stats')
+            
+            heroes.appendChild(article)
                  })
-            .catch((err) =>{})
+            .catch((err) =>{
+            })
             }
             
     }
